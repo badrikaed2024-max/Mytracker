96ed88c1d8909b0e7452deca8d89f79d31c91e00
badrikaed2024-max <badrikaed2024@gmail.com>
2026-08-29 00:33:22 +0300

    Initialize Express server with basic routes

---
+++ server .js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Server is running successfully!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
